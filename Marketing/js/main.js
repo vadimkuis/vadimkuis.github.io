745cbb0a8b43515792b4d6dbc29bf2236cb9cc77
vadimkuis <57957817+vadimkuis@users.noreply.github.com>
2022-09-22 22:20:24 +0300

    Add files via upload

---
+++ Marketing/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+  $(".menu a, .go-top").on("click", function (e) {
+		//отменяем стандартную обработку нажатия по ссылке
+		e.preventDefault();
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+  $('.slider-blog__inner').slick({
+    dots:true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
+  });
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+   
+  var mixer = mixitup('.portfolio__content');
+});
