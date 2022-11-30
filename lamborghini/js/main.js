e60508848341703862cc17e665bbccdb4620de98
vadimkuis <57957817+vadimkuis@users.noreply.github.com>
2022-11-30 16:27:41 +0300

    Add files via upload

---
+++ lamborghini/js/main.js
@@ -0,0 +1,147 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView:2,
+    spaceBetween:20,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.231916093825006, lng: -8.628456218320757 },
+    zoom: 14,
+    styles:[
+      {
+          "featureType": "administrative",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 65
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": "50"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "30"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "40"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffff00"
+              },
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -97
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -100
+              }
+          ]
+      }
+  ]
+  });
+}
+
+window.initMap = initMap;
